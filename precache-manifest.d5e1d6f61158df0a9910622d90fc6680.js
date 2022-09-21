f42d5220f99b7b721e1639e73973a70deaf6d452
riyaV3028 <meriyaverma02@gmail.com>
2022-09-22 01:10:24 +0530

    Updates

---
+++ precache-manifest.d5e1d6f61158df0a9910622d90fc6680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0744e0d3ffa199b946d",
+    "url": "/textutils/static/css/main.212687d4.chunk.css"
+  },
+  {
+    "revision": "c0744e0d3ffa199b946d",
+    "url": "/textutils/static/js/main.c0744e0d.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/textutils/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "d3687133325be23f7e7a",
+    "url": "/textutils/static/js/2.d3687133.chunk.js"
+  },
+  {
+    "revision": "630f10c0371371d002cf",
+    "url": "/textutils/static/js/runtime~main.630f10c0.js"
+  },
+  {
+    "revision": "531907b81f5ae675f052d7178275b260",
+    "url": "/textutils/index.html"
+  }
+];
